7f40bc0e267b602fd43cad379c71d2df9a04c9c1
Alisdair Rankine <rankinesoftware@gmail.com>
2018-05-23 21:56:49 +0100

    mostly documented opcodes

--- cpu.go
+++ cpu.go
@@ -82,15 +82,61 @@ func (c *CPU) Execute() {
 		case 0x0003:
 			//set V[X] to V[X] XOR V[Y] (0x8XY3)
 		case 0x0004:
-			//set V[X] to V[X] + V[Y] (0x8XY4), set V[F] to 1 if carry
+			//set V[X] to V[X] + V[Y] (0x8XY4), set V[F] to 1 if carry, otherwise 0
 		case 0x0005:
-			//set V[X] to V[X] - V[Y] (0x8XY5), set V[F] to 1 if no borrow
+			//set V[X] to V[X] - V[Y] (0x8XY5), set V[F] to 1 if no borrow, otherwise 0
 		case 0x0006:
 			//set V[X] to V[Y] >> 1 (0x8XY6), set V[F] to V[Y] LSB before shift
 		case 0x0007:
-			//set V[X] to V[Y] - V[Y] (0x8XY7)
-		case 0x0006:
-			//set V[X] (and V[Y]) to V[Y] << 1 (0x8XY6)
+			//set V[X] to V[Y] - V[Y] (0x8XY7), set V[F] to 1 if no borrow, otherwise 0
+		case 0x000E:
+			//set V[X] (and V[Y]) to V[Y] << 1 (0x8XYE), set V[F] to V[Y] MSB before shift
+		}
+	case 0x9000:
+		//skip next instruction if V[X]!=V[Y] (0x9XY0)
+	case 0xA000:
+		//set I to NNN (0xANNN)
+	case 0xB000:
+		//jump to addr V[0]+NNN: set PC to V[0] +NNN (0xBNNN)
+	case 0xC000:
+		//set V[x] to R+NN where R = random number between 0 and 255(0xCXNN)
+	case 0xD000:
+		//draw sprite at position (V[X],V[Y]) with width 8, heigh N.
+		//sprite bits located at Memory[I] in rows of 8 (0xDXYN)
+		//V[F] is set to 1 if pixels are flipped from 1 to 0, otherwise 0
+	case 0xE000:
+		switch opCode & 0x00FF {
+		case 0x009E:
+		//skip next instruction if key pressed == v[X] (0xEX9E)
+		case 0x00A1:
+			//skip next instruction if key pressed != v[X] (0xEXA1)
+		}
+	case 0xF000:
+		switch opCode & 0x00FF {
+		case 0x0007:
+			//set V[X] to DT (0xFX07)
+		case 0x000A:
+			//set v[X] to key pressed (0xFX0A)
+		case 0x0015:
+			//set DT to V[X] (0xFX15)
+		case 0x0018:
+			//set ST to V[X] (0xFX18)
+		case 0x001E:
+			//set I = I + V[X] (0xFX1E)
+		case 0x0029:
+			//set I to sprite address for glyph of V[X] (4x5 px font) (0xFX29)
+		case 0x0033:
+			//get BCD representation of V[X]
+			//Memory[I+0] = Decimal MSB Digit (3)
+			//Memory[I+1] = Decimal Middle Digit (2)
+			//Memory[I+3] = Decimal LSB Digit (3)
+			//(0xFX33)
+		case 0x0055:
+			//Store V[0] to V[X] (inclusive) at memory location I, increasing I per register
+			//(0xFX55)
+		case 0x0065:
+			//Set V[0] to V[x] (inclusive) to values from location I, increasing I per register
+			//(0xFX65)
 		}
 	}
 }
