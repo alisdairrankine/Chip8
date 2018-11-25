309ac92eccdd9213dea6cfcd436d3e2ab91d7af3
Alisdair Rankine <rankinesoftware@gmail.com>
2018-11-25 19:00:03 +0000

    refactor tests per opcode

--- cpu_test.go
+++ cpu_test.go
@@ -6,23 +6,47 @@ import (
 	"github.com/alisdairrankine/chip8"
 )
 
-func TestSET(t *testing.T) {
-
+func TestOpCode0NNN(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode00E0(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode00EE(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode1NNN(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode2NNN(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode3XNN(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode4XNN(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode5XY0(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode6XNN(t *testing.T) {
 	cpu := chip8.NewCPU(nil)
 	op := uint16(0x6058)
 	cpu.ExecuteOp(op)
 	if cpu.V[0] != 88 {
 		t.Fail()
 	}
-
-	op = uint16(0x8100)
-	cpu.ExecuteOp(op)
-	if cpu.V[1] != 88 {
-		t.Fail()
-	}
 }
-
-func TestMaths(t *testing.T) {
+func TestOpCode7XNN(t *testing.T) {
 	cpu := chip8.NewCPU(nil)
 	op := uint16(0x7008)
 	cpu.ExecuteOp(op)
@@ -30,8 +54,24 @@ func TestMaths(t *testing.T) {
 	if cpu.V[0] != 16 {
 		t.Fail()
 	}
-
-	op = uint16(0x8011)
+}
+func TestOpCode8XY0(t *testing.T) {
+	cpu := chip8.NewCPU(nil)
+	op := uint16(0x6058)
+	cpu.ExecuteOp(op)
+	if cpu.V[0] != 88 {
+		t.Fail()
+	}
+	op = uint16(0x8100)
+	cpu.ExecuteOp(op)
+	if cpu.V[1] != 88 {
+		t.Fail()
+	}
+	t.Skip()
+}
+func TestOpCode8XY1(t *testing.T) {
+	cpu := chip8.NewCPU(nil)
+	op := uint16(0x8011)
 	cpu.V[0] = 0x55
 	cpu.V[1] = 0x02
 	cpu.ExecuteOp(op)
@@ -39,7 +79,10 @@ func TestMaths(t *testing.T) {
 		t.Fail()
 	}
 
-	op = uint16(0x8012)
+}
+func TestOpCode8XY2(t *testing.T) {
+	cpu := chip8.NewCPU(nil)
+	op := uint16(0x8012)
 	cpu.V[0] = 0x55
 	cpu.V[1] = 0xFE
 	cpu.ExecuteOp(op)
@@ -47,15 +90,20 @@ func TestMaths(t *testing.T) {
 		t.Fail()
 	}
 
-	op = uint16(0x8013)
+}
+func TestOpCode8XY3(t *testing.T) {
+	cpu := chip8.NewCPU(nil)
+	op := uint16(0x8013)
 	cpu.V[0] = 0x55
 	cpu.V[1] = 0xFF
 	cpu.ExecuteOp(op)
 	if cpu.V[0] != 0xAA {
 		t.Fail()
 	}
-
-	op = uint16(0x8014)
+}
+func TestOpCode8XY4(t *testing.T) {
+	cpu := chip8.NewCPU(nil)
+	op := uint16(0x8014)
 	cpu.V[0] = 0xFE
 	cpu.V[1] = 0x01
 	cpu.ExecuteOp(op)
@@ -70,16 +118,16 @@ func TestMaths(t *testing.T) {
 	cpu.V[1] = 0x02
 	cpu.ExecuteOp(op)
 	if cpu.V[0] != 0x01 || (cpu.V[0xF]&0x01) != 1 {
-		t.Log("0x8014 failed")
 		t.Fail()
 	}
-
-	op = uint16(0x8015)
+}
+func TestOpCode8XY5(t *testing.T) {
+	cpu := chip8.NewCPU(nil)
+	op := uint16(0x8015)
 	cpu.V[0] = 0xFF
 	cpu.V[1] = 0x02
 	cpu.ExecuteOp(op)
 	if cpu.V[0] != 0xFD || (cpu.V[0xF]&0x01) != 0x01 {
-		t.Log("0x8015(3) failed")
 		t.Fail()
 	}
 
@@ -91,9 +139,80 @@ func TestMaths(t *testing.T) {
 	// 	t.Log("0x8015(2) failed")
 	// 	t.Fail()
 	// }
-
 }
-
-func TestGraphics(t *testing.T) {
+func TestOpCode8XY6(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode8XY7(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode8XYE(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCode9XY0(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeANNN(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeDXYN(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeBNNN(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeCXNN(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeEX9E(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeEXA1(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeFX07(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeFX0A(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeFX15(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeFX18(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeFX1E(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeFX29(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeFX33(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeFX55(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
+	t.Skip()
+}
+func TestOpCodeFX65(t *testing.T) {
+	//cpu := chip8.NewCPU(nil)
 	t.Skip()
 }
